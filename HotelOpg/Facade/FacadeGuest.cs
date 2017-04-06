d2f123f064eb5cf01ffca67d2ba09933b1a4600c
Dshy <KortSkg@KORTSKG-PC>
2017-04-06 13:51:26 +0200

    Ting virker sådan da

---
+++ HotelOpg/Facade/FacadeGuest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HotelOpg.Model;
+using Windows.UI.Notifications;
+
+namespace HotelOpg.Facade
+{
+    public static class GuestFacade
+    {
+        const string serverUrl = "http://localhost:4284/";
+        public static string messageError = "";
+
+        public static void getGuest()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                string urlString = "api/Guests/";
+
+                HttpResponseMessage response = client.GetAsync(urlString).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var Guest = response.Content.ReadAsAsync<Guest>().Result;
+
+                    return Guest;
+                }
+
+            }
+
+        }
+
+    }
+}
--- HotelOpg/Model/GuestSingelton.cs
+++ HotelOpg/Model/GuestSingelton.cs
@@ -55,4 +55,5 @@ namespace HotelOpg.Model
         {
             GuestList.Remove(gv);
         }
+    }
 }
